d0912264f02cd5e54fa7fdd71a0119e90823bab1
Yuriy Kots <yuriy@gestalt.io>
2018-03-12 23:11:09 -0400

    Add class to header on window scroll; add css transition

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,6 @@
-console.log("Hello Yuriy")
+import header from './interface/header';
+
+document.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+    header.init();
+});
---
+++ static/js/interface/header.js
@@ -0,0 +1,11 @@
+export default {
+    init() {
+        document.addEventListener('scroll', function(e) {
+            if (e.path[1].scrollY > 40) {
+                document.querySelector('[data-header]').classList.add('white');
+            } else {
+                document.querySelector('[data-header]').classList.remove('white');
+            }
+        });
+    }
+};
