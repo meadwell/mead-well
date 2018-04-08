af879555c37cc6d5b3686d637682f193d12b5e8f
Yuriy Kots <yuriy@gestalt.io>
2018-04-08 11:36:23 -0400

    Add instagram section

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,10 @@
 import header from './interface/header';
 import carousel from './sections/carousel';
+import instagram from './sections/instagram';
 
 document.addEventListener('DOMContentLoaded', function() {
     'use strict';
     header.init();
     carousel.init();
+    instagram.init();
 });
