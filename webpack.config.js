33abda5818ef9b7b5d2c648a668b313affe0a492
Yuriy Kots <yuriy@gestalt.io>
2018-11-25 09:41:05 -0500

    Remove web-pack dependency; add script.js

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    entry: './static/js/app.js',
-    output: {
-        path: path.resolve(__dirname, 'static/dist'),
-        filename: 'app.bundle.js'
-    }
-};
